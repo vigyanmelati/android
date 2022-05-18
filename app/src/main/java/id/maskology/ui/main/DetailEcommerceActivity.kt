37372772f2fa8f2e4503995062d4523c9b60ecaf
vigyanmelati <gyanixc11@gmail.com>
2022-05-18 20:52:00 +0800

    Membuat Xml activity login, detail Ecommerce, camera, dan story product

---
+++ app/src/main/java/id/maskology/ui/main/DetailEcommerceActivity.kt
@@ -0,0 +1,12 @@
+package id.maskology.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.maskology.R
+
+class DetailEcommerceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_ecommerce)
+    }
+}
---
+++ app/src/main/java/id/maskology/ui/main/StoryProductActivity.kt
@@ -0,0 +1,12 @@
+package id.maskology.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.maskology.R
+
+class StoryProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_product)
+    }
+}
